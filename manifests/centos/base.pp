bb3ec6d2048fdd46f920cae0dba4f893225ee547
Kimo Rosenbaum <kimor79@yahoo.com>
2015-01-15 22:29:55 -0800

    Add wrappers for base centos and epel repos

---
+++ manifests/centos/base.pp
@@ -0,0 +1,66 @@
+# CentOS Base repos
+class yumrepo::centos::base (
+  $base_baseurl          = undef,
+  $base_mirrorlist       = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os',
+  $centosplus_baseurl    = undef,
+  $centosplus_mirrorlist = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus',
+  $contrib_baseurl       = undef,
+  $contrib_mirrorlist    = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=contrib',
+  $extras_baseurl        = undef,
+  $extras_mirrorlist     = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras',
+  $updates_baseurl       = undef,
+  $updates_mirrorlist    = 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates',
+) {
+
+  yumrepo::repo { 'centos-base':
+    baseurl    => $base_baseurl,
+    descr      => 'CentOS-$releasever - Base',
+    file       => 'CentOS-Base',
+    mirrorlist => $base_mirrorlist,
+    gpgcheck   => '1',
+    gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::operatingsystemmajrelease}",
+    order      => '10',
+  }
+  
+  yumrepo::repo { 'centos-updates':
+    baseurl    => $updates_baseurl,
+    descr      => 'CentOS-$releasever - Updates',
+    file       => CentOS-Base,
+    mirrorlist => $updates_mirrorlist,
+    gpgcheck   => '1',
+    gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::operatingsystemmajrelease}",
+    order      => '20',
+  }
+  
+  yumrepo::repo { 'centos-extras':
+    baseurl    => $extras_baseurl,
+    descr      => 'CentOS-$releasever - Extras',
+    file       => 'CentOS-Base',
+    mirrorlist => $extras_mirrorlist,
+    gpgcheck   => '1',
+    gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::operatingsystemmajrelease}",
+    order      => '30',
+  }
+
+  yumrepo::repo { 'centos-centosplus':
+    baseurl    => $centosplus_baseurl,
+    descr      => 'CentOS-$releasever - Plus',
+    file       => 'CentOS-Base',
+    mirrorlist => $centosplus_mirrorlist,
+    gpgcheck   => '1',
+    enabled    => '0',
+    gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::operatingsystemmajrelease}",
+    order      => '40',
+  }
+
+  yumrepo::repo { 'centos-contrib':
+    baseurl    => $contrib_baseurl,
+    descr      => 'CentOS-$releasever - Contrib',
+    file       => 'CentOS-Base',
+    mirrorlist => $contrib_mirrorlist,
+    gpgcheck   => '1',
+    enabled    => '0',
+    gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::operatingsystemmajrelease}",
+    order      => '50',
+  }
+}
---
+++ manifests/epel.pp
@@ -0,0 +1,46 @@
+# Epel repos
+class yumrepo::epel (
+  $debug_baseurl     = undef,
+  $debug_mirrorlist  = "https://mirrors.fedoraproject.org/metalink?repo=epel-debug-${::operatingsystemmajrelease}&arch=\$basearch",
+  $epel_baseurl      = undef,
+  $epel_mirrorlist   = "https://mirrors.fedoraproject.org/metalink?repo=epel-${::operatingsystemmajrelease}&arch=\$basearch",
+  $source_baseurl    = undef,
+  $source_mirrorlist = "https://mirrors.fedoraproject.org/metalink?repo=epel-source-${::operatingsystemmajrelease}&arch=\$basearch",
+) {
+
+  yumrepo::repo { 'epel':
+    baseurl        => $epel_baseurl,
+    descr          => "Extra Packages for Enterprise Linux %{::operatingsystemmajrelease} - \$basearch",
+    file           => epel,
+    mirrorlist     => $epel_mirrorlist,
+    failovermethod => 'priority',
+    enabled        => '1',
+    gpgcheck       => '1',
+    gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${::operatingsystemmajrelease}",
+    order          => '10',
+  }
+
+  yumrepo::repo { 'epel-debuginfo':
+    baseurl        => $debuginfo_baseurl,
+    descr          => "Extra Packages for Enterprise Linux ${::operatingsystemmajrelease} - \$basearch - Debug",
+    file           => epel,
+    mirrorlist     => $debuginfo_mirrorlist,
+    failovermethod => 'priority',
+    enabled        => '0',
+    gpgcheck       => '1',
+    gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${::operatingsystemmajrelease}",
+    order          => '11',
+  }
+
+  yumrepo::repo { 'epel-source':
+    baseurl        => $source_baseurl,
+    descr          => "Extra Packages for Enterprise Linux ${::operatingsystemmajrelease} - \$basearch - Source",
+    file           => epel,
+    mirrorlist     => $source_mirrorlist,
+    failovermethod => 'priority',
+    enabled        => '0',
+    gpgcheck       => '1',
+    gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${::operatingsystemmajrelease}",
+    order          => '12',
+  }
+}
