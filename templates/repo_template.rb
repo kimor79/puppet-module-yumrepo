f29ff46620052a6dbed27caa8bfee70d8e3c722f
Kimo Rosenbaum <kimor79@yahoo.com>
2016-02-22 22:24:01 -0800

    Add more options from yum.conf

--- templates/repo_template.rb
+++ templates/repo_template.rb
@@ -1,4 +1,7 @@
 [<%= @name %>]
+<% if @async && @async != '' -%>
+async=<%= @async %>
+<% end -%>
 <% if @bandwidth && @bandwidth != '' -%>
 bandwidth=<%= @bandwidth %>
 <% end -%>
@@ -7,6 +10,12 @@ baseurl=<%= @baseurl %>
 <% end -%>
 <% if @cost && @cost != '' -%>
 cost=<%= @cost %>
+<% end -%>
+<% if @deltarpm_metadata_percentage && @deltarpm_metadata_percentage != '' -%>
+cost=<%= @deltarpm_metadata_percentage %>
+<% end -%>
+<% if @deltarpm_percentage && @deltarpm_percentage != '' -%>
+cost=<%= @deltarpm_percentage %>
 <% end -%>
 <% if @descr&& @descr != ''  -%>
 name=<%= @descr %>
@@ -41,14 +50,23 @@ include=<%= @include %>
 <% if @includepkgs && @includepkgs != '' -%>
 includepkgs=<%= @includepkgs %>
 <% end -%>
+<% if @ip_resolve && @ip_resolve != '' -%>
+ip_resolve=<%= @ip_resolve %>
+<% end -%>
 <% if @keepalive && @keepalive != '' -%>
 keepalive=<%= @keepalive %>
+<% end -%>
+<% if @keepcache && @keepcache != '' -%>
+keepcache=<%= @keepcache %>
 <% end -%>
 <% if @key_id && @key_id != '' -%>
 key_id=<%= @key_id %>
 <% end -%>
 <% if @metadata_expire && @metadata_expire != '' -%>
 metadata_expire=<%= @metadata_expire %>
+<% end -%>
+<% if @metadata_expire_filter && @metadata_expire_filter != '' -%>
+metadata_expire_filter=<%= @metadata_expire_filter %>
 <% end -%>
 <% if @metalink && @metalink != '' -%>
 metalink=<%= @metalink %>
@@ -58,6 +76,9 @@ mirrorlist=<%= @mirrorlist %>
 <% end -%>
 <% if @mirrorlist_expire && @mirrorlist_expire != '' -%>
 mirrorlist_expire=<%= @mirrorlist_expire %>
+<% end -%>
+<% if @password && @password != '' -%>
+password=<%= @password %>
 <% end -%>
 <% if @priority && @priority != '' -%>
 priority=<%= @priority %>
@@ -77,6 +98,9 @@ proxy_password=<%= @proxy_password %>
 <% if @proxy_username && @proxy_username != '' -%>
 proxy_username=<%= @proxy_username %>
 <% end -%>
+<% if @repo_gpgcheck && @repo_gpgcheck != '' -%>
+repo_gpgcheck=<%= @repo_gpgcheck %>
+<% end -%>
 <% if @retries && @retries != '' -%>
 retries=<%= @retries %>
 <% end -%>
@@ -88,6 +112,9 @@ s3_enabled=<%= @s3_enabled %>
 <% end -%>
 <% if @skip_if_unavailable && @skip_if_unavailable != '' -%>
 skip_if_unavailable=<%= @skip_if_unavailable %>
+<% end -%>
+<% if @ssl_check_cert_permissions && @ssl_check_cert_permissions != '' -%>
+ssl_check_cert_permissions=<%= @ssl_check_cert_permissions %>
 <% end -%>
 <% if @sslcacert && @sslcacert != '' -%>
 sslcacert=<%= @sslcacert %>
@@ -109,4 +136,10 @@ throttle=<%= @throttle %>
 <% end -%>
 <% if @timeout && @timeout != '' -%>
 timeout=<%= @timeout %>
+<% end -%>
+<% if @ui_repoid_vars && @ui_repoid_vars != '' -%>
+ui_repoid_vars=<%= @ui_repoid_vars %>
+<% end -%>
+<% if @username && @username != '' -%>
+username=<%= @username %>
 <% end -%>
